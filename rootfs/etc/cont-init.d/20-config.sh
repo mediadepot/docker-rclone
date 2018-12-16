7e2ceed8cd5cc824ec859ec0a842cafdbd56b945
Jason Kulatunga <jason@thesparktree.com>
2018-12-16 10:05:00 -0800

    backup daily. mkdir destination to ensure it exists. redirect rclone sync to stdout.

--- rootfs/etc/cont-init.d/20-config.sh
+++ rootfs/etc/cont-init.d/20-config.sh
@@ -7,9 +7,9 @@ mkdir -p \
 
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-if [[ ! -L /etc/periodic/15min/rclone && ! -f /srv/rclone/config/cron-15mins ]]; then
+if [[ ! -L /etc/periodic/daily/rclone && ! -f /srv/rclone/config/cron-daily ]]; then
 
-cat > /srv/rclone/config/cron-15mins <<DELIM
+cat > /srv/rclone/config/cron-daily <<DELIM
 #!/bin/sh
 # GLOBAL OPTS
 RCLONE_OPTS="--config /srv/rclone/config/rclone.conf"
@@ -18,11 +18,13 @@ SYNC_OPTS=""
 # SYNC ENTRY OPTS
 #SRC="/opt/mediadepot/apps/couchpotato"
 #DEST="gdrive:/depot_backup/couchpotato"
-#echo "INFO: Starting rclone sync \$SRC \$DEST \$RCLONE_OPTS \$SYNC_OPTS"
-#LANG=C.UTF-8 /usr/bin/rclone sync \$SRC \$DEST \$RCLONE_OPTS \$SYNC_OPTS
-DELIM
-chmod +x /srv/rclone/config/cron-15mins
 
-ln -s /srv/rclone/config/cron-15mins /etc/periodic/15min/rclone
+#/usr/bin/rclone mkdir \$DEST \$RCLONE_OPTS
+#echo "INFO: Starting rclone sync \$SRC \$DEST \$RCLONE_OPTS \$SYNC_OPTS"
+#/usr/bin/rclone sync \$SRC \$DEST \$RCLONE_OPTS \$SYNC_OPTS > /dev/stdout
+DELIM
+chmod +x /srv/rclone/config/cron-daily
+
+ln -s /srv/rclone/config/cron-daily /etc/periodic/daily/rclone
 
 fi
